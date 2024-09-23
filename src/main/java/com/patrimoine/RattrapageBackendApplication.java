f0e1b68d49df8636c13d5064a245e36d46c5b47d
Miranyaina <hei.mira.110@gmail.com>
2024-09-23 15:24:01 +0300

    first commit

---
+++ src/main/java/com/patrimoine/RattrapageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.patrimoine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RattrapageBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RattrapageBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/patrimoine/RattrapageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrimoine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RattrapageBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
