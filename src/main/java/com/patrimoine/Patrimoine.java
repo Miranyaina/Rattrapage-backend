e761b701f8899b78cc62c948793bf2029772c26a
Miranyaina <hei.mira.110@gmail.com>
2024-09-23 15:27:42 +0300

    définition de la classe patrimoine

---
+++ src/main/java/com/patrimoine/Patrimoine.java
@@ -0,0 +1,29 @@
+package com.patrimoine;
+
+import java.time.LocalDateTime;
+
+public class Patrimoine {
+    private String possesseur;
+    private LocalDateTime derniereModification;
+
+    public Patrimoine(String possesseur, LocalDateTime derniereModification) {
+        this.possesseur = possesseur;
+        this.derniereModification = derniereModification;
+    }
+
+    public String getPossesseur() {
+        return possesseur;
+    }
+
+    public void setPossesseur(String possesseur) {
+        this.possesseur = possesseur;
+    }
+
+    public LocalDateTime getDerniereModification() {
+        return derniereModification;
+    }
+
+    public void setDerniereModification(LocalDateTime derniereModification) {
+        this.derniereModification = derniereModification;
+    }
+}
